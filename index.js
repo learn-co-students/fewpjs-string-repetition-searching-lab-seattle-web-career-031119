fcf7a21b7c4d839904a8b71a49a89e3301fef24a
256hz <a.d@256hz.com>
2019-05-02 08:18:56 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function repeat(phrase, times) {
+  let response = []
+  for (let i=0; i<times; i++) {
+    response.push(phrase)
+  }
+  return response
+}
+
+
+function heyJude(verse) {
+  return repeat(verse, 16)
+}
+
+function prohibitedLanguage(phrase) {
+  let bad = /candycorn|brusselssprouts/
+  return phrase.search(bad)
+}
